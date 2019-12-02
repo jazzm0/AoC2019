b2fb416fea3c96cd56b499d381bb7c3e82faf100
Tibor Tarnai <tibor.tarnai@sap.com>
2019-12-02 10:29:51 +0100

    day2 #2

--- day2/day2.py
+++ day2/day2.py
@@ -3,29 +3,31 @@ mul = 2
 halt = 99
 
 
-def convert(s):
-    return [int(i) for i in s.split(',')]
-
-
-def calc(op, a, b, s):
+def calc(op, a, b):
     if op == add:
         return a + b
     if op == mul:
         return a * b
 
 
+def process(noun, verb, program):
+    program[1] = noun
+    program[2] = verb
+    index = 0
+    while index + 3 < len(program):
+        operation = program[index]
+        if operation == halt:
+            if program[0] == 19690720:
+                print(100 * noun + verb)
+                exit()
+            print(program[0])
+        program[program[index + 3]] = calc(operation, program[program[index + 1]], program[program[index + 2]])
+        index += 4
+
+
 with open('input') as ifile:
     for line in ifile:
-        line = convert(line)
-        line[1] = 12
-        line[2] = 2
-        index = 0
-        while index < len(line):
-            operation = line[index]
-            if operation == halt:
-                print(line[0])
-                exit()
-            operand_a = line[line[index + 1]]
-            operand_b = line[line[index + 2]]
-            line[line[index + 3]] = calc(operation, operand_a, operand_b, line)
-            index += 4
+        line = [int(i) for i in line.split(',')]
+        for i in range(1, 100):
+            for j in range(1, 100):
+                process(i, j, line.copy())
