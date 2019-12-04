ef0c2c2a197586d6e904586a08f458fff6f04500
Tibor Tarnai <tibor.tarnai@sap.com>
2019-12-04 09:50:57 +0100

    add day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,18 +1,39 @@
-# 172930-683082
+f = 172930
+t = 683082
 
-count = 0
+count_one = 0
+count_two = 0
 
 
-def contains_two(a):
-    c = str(a)
+def part_one(n):
+    contains = False
+    c = str(n)
+    for i in range(len(c) - 1):
+        if not contains and c[i] == c[i + 1]:
+            contains = True
+        if c[i] > c[i + 1]:
+            return False
+    return contains
+
+
+def part_two(n):
+    c = str(n)
+    b = {}
     for i in range(len(c) - 1):
         if c[i] == c[i + 1]:
+            b[c[i]] = b.get(c[i], 1) + 1
+
+    for j in b.values():
+        if j == 2:
             return True
+    return False
 
 
-for a in range(172930, 683082):
-    if contains_two(a):
-        print(a)
-        count += 1
+for a in range(f, t):
+    if part_one(a):
+        count_one += 1
+        if part_two(a):
+            count_two += 1
 
-print(count)
+print(count_one)
+print(count_two)
